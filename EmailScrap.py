1de7b7aef63907ad666f270f49fed0e5ed2ff9fd
Ejan <sthaejan00@gmail.com>
2022-02-24 10:20:13 +0545

    Final Program

--- EmailScrap.py
+++ EmailScrap.py
@@ -1,19 +1,19 @@
+from mailcap import findmatch
 import re
 import json
 from collections import Counter
 file = open('websiteData.txt', 'r', encoding='utf-8')
 f = file.read()
 h = re.findall('[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*', f)
-count = Counter(h)
 emails = {}
 for i in h:
     emails[i] = {}
-    emails[email]['Occurance'] = h.count(i)
+    emails[i]['Occurance'] = h.count(i)
     a = i.split('@')
-    if (len(a[0]) <= 8):
-        emails[i]['EmailType'] = 'Non-human'
-    else:
+    if re.findall('\S+\.\S+@\S+', i) or (len(a[0]) >= 8):
         emails[i]['EmailType'] = 'Human'
+    else:
+        emails[i]['EmailType'] = 'Non-Human'
 print(emails)
-# to_json=open('result.json','w')
-# to_json=json.dump(emails,to_json)
+to_json = open('result.json', 'w')
+to_json = json.dump(emails, to_json)
